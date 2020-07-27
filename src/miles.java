6c3df19a936033b2f906a796144ed75d75ea8a38
Oleg Matveenko <om1190@yandex.ru>
2020-07-27 17:42:46 +0500

    initial commit

---
+++ src/miles.java
@@ -0,0 +1,9 @@
+class miles {
+    public static void main(String[] args) {
+        int price = 5000;
+        int spent = 20;
+        int miles = price / spent;
+
+        System.out.println(miles);
+    }
+}
